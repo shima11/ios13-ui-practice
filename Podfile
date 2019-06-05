d1f19bf837b59c5c96fb4642c262217bea95ab14
Shima <shima.jin@icloud.com>
2019-06-05 16:50:09 -0700

    update

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios13-ui-practice' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ios13-ui-practice
+  
+  pod "EasyPeasy"
+  pod "Reveal-SDK"
+
+end
--- ios13-ui-practice/ViewController.swift
+++ ios13-ui-practice/ViewController.swift
@@ -12,7 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let segment = UISegmentedControl()
+        segment.insertSegment(withTitle: "1", at: 0, animated: false)
+        segment.insertSegment(withTitle: "2", at: 1, animated: false)
+        segment.insertSegment(withTitle: "3", at: 2, animated: false)
+        
+        segment.frame = .init(x: 0, y: 0, width: 120, height: 40)
+        segment.tintColor = .darkGray
+        segment.backgroundColor = .lightGray
+        segment.needsUpdateConstraints()
+        
+        let activity = UIActivityIndicatorView(style: .large)
+        activity.color = .darkGray
+        activity.startAnimating()
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            segment,
+            activity
+            ])
+        stackView.axis = .vertical
+        stackView.frame = view.bounds
+        stackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(stackView)
     }
 
 
