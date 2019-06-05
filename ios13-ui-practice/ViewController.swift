96e1a1b5d3bbe10236fe25a23aa9ac6184ae30ac
Shima <shima.jin@icloud.com>
2019-06-05 16:22:45 -0700

    Initial Commit

---
+++ ios13-ui-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios13-ui-practice
+//
+//  Created by jinsei_shima on 2019/06/05.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
